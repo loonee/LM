b12ae3e9d466c9e9d258d07935af29f61e4b420a
Luniva Maharjan <60943092+loonee@users.noreply.github.com>
2020-05-04 13:38:49 -0500

    Add files via upload

---
+++ IfSimple.java
@@ -0,0 +1,10 @@
+class IfSimple{
+public static void main(String args[]){
+int a,b;
+a=25;
+b=10;
+if(a>b){
+System.out.println("a is greater than b");
+}
+}
+}
---
+++ IfelseSimple.java
@@ -0,0 +1,13 @@
+class IfelseSimple{
+public static void main(String args[]){
+int a,b;
+a=25;
+b=26;
+if(a>b){
+System.out.println("a is greater than b.");
+}
+else{
+System.out.println("b is greater than a.");
+}
+}
+}
